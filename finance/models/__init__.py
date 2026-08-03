5fecc46a8d36cb95de4435a7f46c7c1316aa9698
MahdiMohammadiME <mohammadi.mahdi.me@gmail.com>
2026-08-03 23:16:57 +0330

    refactor: modifying the finance app models

---
+++ finance/models/__init__.py
@@ -0,0 +1,7 @@
+from finance.models.wage import Wage
+from finance.models.wage_rate import WageRate
+
+__all__ = [
+    "Wage",
+    "WageRate",
+]
--- finance/models/wage.py
+++ finance/models/wage.py
@@ -5,45 +5,49 @@ from system.models import BaseModel
 
 class Wage(BaseModel):
     class MonthChoices(models.IntegerChoices):
-        JAN = 1, "January"
-        FEB = 2, "February"
-        MAR = 3, "March"
-        APR = 4, "April"
-        MAY = 5, "May"
-        JUN = 6, "June"
-        JUL = 7, "July"
-        AUG = 8, "August"
-        SEP = 9, "September"
-        OCT = 10, "October"
-        NOV = 11, "November"
-        DEC = 12, "December"
+        MONTH_01 = 1, "January"
+        MONTH_02 = 2, "February"
+        MONTH_03 = 3, "March"
+        MONTH_04 = 4, "April"
+        MONTH_05 = 5, "May"
+        MONTH_06 = 6, "June"
+        MONTH_07 = 7, "July"
+        MONTH_08 = 8, "August"
+        MONTH_09 = 9, "September"
+        MONTH_10 = 10, "October"
+        MONTH_11 = 11, "November"
+        MONTH_12 = 12, "December"
 
-    teacher_profile = models.ForeignKey("account.TeacherProfile", models.CASCADE)
+    teacher_profile = models.ForeignKey(
+        "account.TeacherProfile",
+        models.CASCADE,
+        related_name="wage",
+    )
     year = models.SmallIntegerField()
     # [validation] shall be greater than the current year
     # this validation is not good, to be able to import previous records
-    month = models.SmallIntegerField(choices=MonthChoices.choices)
-    calculated_by_system = models.DecimalField(max_digits=11, decimal_places=2)
+    month = models.PositiveSmallIntegerField(choices=MonthChoices)
+    amount = models.DecimalField(max_digits=11, decimal_places=2)
     # 999_999_999.99
-    is_modified = models.BooleanField(default=False)
-    modified_by = models.ForeignKey(
-        "account.User",
-        models.CASCADE,
-        null=True,
-        blank=True,
-    )
-    # [validation] if `is_modified==True` then this field is required
-    calculated_by_finance_officer = models.DecimalField(
-        max_digits=11,
-        decimal_places=2,
-        null=True,
-        blank=True,
-    )
-    # 999_999_999.99
-    # [validation] if `is_modified==True` then this field is required
-    # [permission] only users with FIO role
-    modification_reason = models.TextField(blank=True)
-    # [validation] if `is_modified==True` then this field is required
+    # is_modified = models.BooleanField(default=False)
+    # modified_by = models.ForeignKey(
+    #     "account.User",
+    #     models.CASCADE,
+    #     null=True,
+    #     blank=True,
+    # )
+    # # [validation] if `is_modified==True` then this field is required
+    # calculated_by_finance_officer = models.DecimalField(
+    #     max_digits=11,
+    #     decimal_places=2,
+    #     null=True,
+    #     blank=True,
+    # )
+    # # 999_999_999.99
+    # # [validation] if `is_modified==True` then this field is required
+    # # [permission] only users with FIO role
+    # modification_reason = models.TextField(blank=True)
+    # # [validation] if `is_modified==True` then this field is required
 
     def __str__(self) -> str:
-        return f"{self.year}-{self.get_month_display()}"  # type: ignore
+        return f"{self.year}-{self.month.label}"  # type: ignore
