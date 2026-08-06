8db81935bfd5cab267d63defe2c870aaabed9f00
MahdiMohammadiME <mohammadi.mahdi.me@gmail.com>
2026-08-06 16:15:35 +0330

    refactor: improve create_user management command

--- account/management/commands/create_user.py
+++ account/management/commands/create_user.py
@@ -1,21 +1,27 @@
-from django.contrib.auth import get_user_model
+from typing import Any
+
+from django.core.exceptions import ValidationError
 from django.core.management import CommandError, CommandParser
 from django.core.management.base import BaseCommand
-from django.db import IntegrityError
+from django.core.validators import validate_email
 
-USER = get_user_model()
+from account.models import User
+
+USER = User
+# since this is a project not a framework and it is not going to be extended,
+# the `User` model is directly imported - `get_user_model()` has been removed!
 
 
 class Command(BaseCommand):
     help = "Creates a new user"
 
     def add_arguments(self, parser: CommandParser) -> None:
-        option = {"type": str, "required": True}
-        role_option = {**option, "choices": USER.RoleChoices}
+        option: dict[str, Any] = {"type": str, "required": True}
+        role_option: dict[str, Any] = {**option, "choices": USER.RoleChoices}
 
-        parser.add_argument("-u", "--username", help="Set the username", **option)  # type: ignore
-        parser.add_argument("-p", "--password", help="Set the password", **option)  # type: ignore
-        parser.add_argument("-r", "--role", help="Set the role", **role_option)  # type: ignore
+        parser.add_argument("-e", "--email", help="Set the email", **option)
+        parser.add_argument("-p", "--password", help="Set the password", **option)
+        parser.add_argument("-r", "--role", help="Set the role", **role_option)
 
     def handle(self, *args, **options) -> None:
         # [hint] a `try-except` block was used to handle the exceptions, but,
@@ -35,19 +41,25 @@ class Command(BaseCommand):
 
         # default: the first user shall be the ADMIN
         # default: this is the only user with ADMIN role
-        admin = USER.objects.get(role=USER.RoleChoices.ADMIN)
+        # admin = USER.objects.get(role=USER.RoleChoices.ADMIN)
+        # [FINAL DECISION] all users are made by admin, so, it is not required to set
+        # `created_by` or `updated_by`. this is the reason for changing the `User` model.
 
-        if USER.objects.filter(username=options["username"]).exists():
-            raise CommandError("Username is not available!")
+        try:
+            email = options["email"]
+            validate_email(email)
+        except ValidationError:
+            raise CommandError("Invalid email address!")
+
+        if USER.objects.filter(email=email).exists():
+            raise CommandError("Email is not available!")
             # raising exception is useful in tests
 
         new_user = USER.objects.create_user(
-            username=options["username"],
-            password=options["password"],
+            email,
+            options["password"],
             role=options["role"],
-            created_by=admin,
-            updated_by=admin,
         )
 
-        message = f"'{new_user.username}' with 'id={new_user.id}' created successfully."  # type: ignore
+        message = f"'{new_user.email}' with 'id={new_user.id}' created successfully."  # type: ignore
         self.stdout.write(self.style.SUCCESS(message))
