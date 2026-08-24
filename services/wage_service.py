93ec96f72b0ada8213a7cd812f2555fe74a8bfde
MahdiMohammadiME <mohammadi.mahdi.me@gmail.com>
2026-08-24 09:57:30 +0330

    feat: add WageService structure

---
+++ services/wage_service.py
@@ -0,0 +1,18 @@
+class WageService:
+    @classmethod
+    def calculate_wages(cls) -> None:
+        """
+        This method is the entrypoint of this service.
+        """
+
+        # set the corresponding semester
+
+        # set the starting and ending dates
+
+        # filter the sessions
+
+        # are reports reviewed?
+
+        # calculate wage per teacher
+
+        # insert into db
