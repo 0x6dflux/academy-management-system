332ad2ec994543d9f75ac22c608e773f32c4003d
MahdiMohammadiME <mohammadi.mahdi.me@gmail.com>
2026-08-24 10:03:05 +0330

    feat: set the starting and ending dates

--- services/wage_service.py
+++ services/wage_service.py
@@ -1,11 +1,22 @@
+from datetime import date
+
 from education.models import Semester
 
 
 class WageService:
     semester: Semester
+    starting_date: date
+    ending_date: date
 
     @classmethod
-    def calculate_wages(cls, semester: Semester) -> None:
+    def _set_date_range(cls, year: int, month: int) -> None:
+        """This method sets the `[starting, ending)` dates."""
+
+        cls.starting_date = date(year, month, 1)
+        cls.ending_date = date(year, month + 1, 1)
+
+    @classmethod
+    def calculate_wages(cls, semester: Semester, year: int, month: int) -> None:
         """
         This method is the entrypoint of this service.
         """
@@ -14,6 +25,7 @@ class WageService:
         cls.semester = semester
 
         # set the starting and ending dates
+        cls._set_date_range(year, month)
 
         # filter the sessions
 
